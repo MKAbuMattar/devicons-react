fc3aab54f65ca1f0bf1bc12479e5dbc96b0b5d7c
MKAbuMattar <mohammad.khaled@outlook.com>
2022-08-03 16:30:45 +0300

    add pkg: recursive-directory

--- generate/build.config.js
+++ generate/build.config.js
@@ -1,8 +1,8 @@
-const getFiles = require('node-recursive-directory')
+const recursiveDirectory = require('recursive-directory')
 const fs = require('fs')
 const path = require('path')
 
 ;(async () => {
-  const files = await getFiles('./devicon/icons/', true)
+  const files = await recursiveDirectory('./devicon/icons/', true)
   fs.writeFileSync(path.resolve(__dirname, 'build.config.json'), JSON.stringify(files))
 })()
