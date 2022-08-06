aaf96cb780f89fa67195996b1962e4ee304f4ab5
MKAbuMattar <mohammad.khaled@outlook.com>
2022-08-06 19:18:51 +0300

    add: icons [Angular, Apachespark, Fastify, Opentelemetry]

--- generate/build.config.js
+++ generate/build.config.js
@@ -1,8 +1,9 @@
-const recursiveDirectory = require('recursive-directory')
+const _recursiveDirectory = require('recursive-directory')
 const fs = require('fs')
 const path = require('path')
 
 ;(async () => {
-  const files = await recursiveDirectory.recursiveDirectory('./devicon/icons/', true)
+  const { recursiveDirectory } = _recursiveDirectory
+  const files = await recursiveDirectory('./devicon/icons/', true)
   fs.writeFileSync(path.resolve(__dirname, 'build.config.json'), JSON.stringify(files))
 })()
