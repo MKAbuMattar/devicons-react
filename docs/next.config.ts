d1a12ed8be96b3974c7b10d7d55e2dbcca1f29d7
MKAbuMattar <mohammad.khaled@outlook.com>
2024-12-27 16:23:07 +0300

    icons: [Firebird, Kubeflow, Mattermost, Memcached]

---
+++ docs/next.config.ts
@@ -0,0 +1,33 @@
+import type {NextConfig} from 'next';
+import PWANextInit from 'next-pwa';
+import runtimeCachingConfig from 'next-pwa/cache.js';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+} satisfies NextConfig;
+
+const PWANextConfig = PWANextInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: runtimeCachingConfig,
+  publicExcludes: [
+    '!robots.txt',
+    '!sitemap.xml',
+    '!sitemap.*.xml',
+    '!app-build-manifest.json',
+  ],
+  buildExcludes: ['app-build-manifest.json'],
+});
+
+export default PWANextConfig(nextConfig);
